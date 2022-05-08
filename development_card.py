3d7b32a6135dda1ef631c98eb2a49a6a5c3c4751
achiar99 <34912231+achiar99@users.noreply.github.com>
2022-05-08 14:53:48 +0300

    Create development_card.py

---
+++ development_card.py
@@ -0,0 +1,9 @@
+from collections import Counter
+from resource import Resources
+
+class DevelopmentCard:
+    def __init__(self, score: int, gift: Resources, cost: Counter, level: int) -> None:
+        self.score: int = score
+        self.gift: Resources = gift
+        self.cost: Counter = cost
+        self.level: int = level
